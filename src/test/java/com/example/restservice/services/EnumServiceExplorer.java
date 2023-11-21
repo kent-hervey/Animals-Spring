9dc6c74804dbb4f176c890cb627942b1f93dc301
Kent Hervey <kent.hervey@gmail.com>
2023-11-21 16:17:02 -0600

    copied in three files from refactor interface branch AnimalController, EnumServiceExplorer, EnumService; these three files added a controller handler method to obtain list of kinds and also changed routes for controller endpoints that provide feedType

---
+++ src/test/java/com/example/restservice/services/EnumServiceExplorer.java
@@ -0,0 +1,23 @@
+package com.example.restservice.services;
+
+import com.example.restservice.enums.Kind;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class EnumServiceExplorer {
+    //Note the below is not literally a unit test.  It is used for exploration or dry runs. One reason is that we are using the actual Kinds enum, not a mock.
+    @Test
+    void exploreKinds() {
+        EnumService enumService = new EnumService();
+        List<Kind> result = enumService.fetchKinds();
+        result.stream().forEach(kind -> System.out.print("Here is the result:  " + kind));
+    }
+
+    //Note the below is not literally a unit test.  It is used for exploration or dry runs. One reason is that we are using the actual Kinds enum, not a mock.
+    @Test
+    void exploreKindsStrings() {
+        EnumService enumService = new EnumService();
+        String result = enumService.fetchKindsStrings();
+        System.out.println("Here is the result:  " + result);
+    }
+}
